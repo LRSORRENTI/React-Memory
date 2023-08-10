577c04bf992a193faf607cc74b00339912ac7826
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-08-10 16:55:36 -0500

    added cardImages array housing the images, and a shuffleCards function, with a spread array housing those cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 
 import './App.css';
 
+const cardImages = [
+  { "src": "/images/angular.png" },
+  { "src": "/images/django.png" },
+  { "src": "/images/laravel.png" },
+  { "src": "/images/react.png" },
+  { "src": "/images/rubyonrails.png" },
+  { "src": "/images/vue.png" },
+];
+
+
 function App() {
+
+  // shuffle the cards 
+  const shuffleCards = () => {
+    // inside the shuffle cards function, we add 
+    // a new array shuffled, and we spread using ... 
+    // the cardImages, which essentially gives us 
+    // the same as what we see in cardImages 
+    const shuffledCards = [...cardImages]
+  }
+
+
   return (
     <div className='App'>
      <h1>Memory Match</h1>
